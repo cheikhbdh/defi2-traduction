c5f96dfc2700291e281a84bbfd11cf28095f24e5
cheikh202 <cheikh202>
2025-03-16 21:22:06 +0000

    gesion modertaeur

---
+++ src/app/api/get-word/route.ts
@@ -0,0 +1,23 @@
+import { createClient } from '@supabase/supabase-js';
+import { NextResponse } from 'next/server';
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+
+export async function GET() {
+  try {
+    const { data, error } = await supabase
+      .from('words')
+      .select('*')
+      .eq('status', 'en_cours');
+
+    if (error) {
+      console.error('Error fetching terms:', error);
+      return NextResponse.json({ error: 'Failed to fetch terms' }, { status: 500 });
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (err) {
+    console.error('Unexpected error:', err);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
